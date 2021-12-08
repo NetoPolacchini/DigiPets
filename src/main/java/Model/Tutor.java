d53f138ba2a141925c21db87949120e333d1f90f
DrLighting <95546918+DrLighting@users.noreply.github.com>
2021-12-08 04:23:38 -0300

    Update Tutor.java

--- src/main/java/Model/Tutor.java
+++ src/main/java/Model/Tutor.java
@@ -7,7 +7,7 @@ public class Tutor extends Usuario {
     private int cod_tutor;
 
     public Tutor(int cod_tutor, String nome, String cpf, String senha, String tipo_usuario) {
-        super(nome, email, telefone, endereco, cpf, senha, tipo_usuario);
+        super(nome, cpf, senha, tipo_usuario);
         this.cod_tutor = cod_tutor;
     }
 
