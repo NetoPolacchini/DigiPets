17278eeb52ed266fddc7e192fd48ccfaa5d821af
DrLighting <95546918+DrLighting@users.noreply.github.com>
2021-12-08 04:21:55 -0300

    Update Usuario.java

--- src/main/java/Model/Usuario.java
+++ src/main/java/Model/Usuario.java
@@ -5,24 +5,17 @@ package Model;
 public class Usuario {
     
     private String nome;
-    private String email;
-    private String telefone;
-    private String endereco;
     private String cpf;
     private String senha;
     private String tipo_usuario;
 
-    public Usuario(String nome, String email, String telefone, String endereco, String cpf, String senha, String tipo_usuario) {
+    public Usuario(String nome, String cpf, String senha, String tipo_usuario) {
         this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
-        this.endereco = endereco;
         this.cpf = cpf;
         this.senha = senha;
         this.tipo_usuario = tipo_usuario;
     }
-    
-    
+   
 
     public String getNome() {
         return nome;
@@ -30,30 +23,6 @@ public class Usuario {
 
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
     }
 
     public String getCpf() {
