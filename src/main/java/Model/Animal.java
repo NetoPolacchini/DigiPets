fb67c4f19059b8b7bf4f41ffdc5324d106ec7336
DrLighting <95546918+DrLighting@users.noreply.github.com>
2021-12-08 04:20:12 -0300

    Update Animal.java

--- src/main/java/Model/Animal.java
+++ src/main/java/Model/Animal.java
@@ -8,22 +8,15 @@ public class Animal {
     
     private String nome_animal;
     private int cod_animal;
-    private Date data_nasc_animal;
-    private String sexo_animal;
-    private String especie_animal;
     private String raca_animal;
-    private float peso_animal;
-    private String obs_gerais;
+    private String sexo_animal;
 
-    public Animal(String nome_animal, int cod_animal, Date data_nasc_animal, String sexo_animal, String especie_animal, String raca_animal, float peso_animal, String obs_gerais) {
+    public Animal(String nome_animal, int cod_animal, String sexo_animal, String raca_animal) {
         this.nome_animal = nome_animal;
         this.cod_animal = cod_animal;
         this.data_nasc_animal = data_nasc_animal;
         this.sexo_animal = sexo_animal;
         this.especie_animal = especie_animal;
-        this.raca_animal = raca_animal;
-        this.peso_animal = peso_animal;
-        this.obs_gerais = obs_gerais;
     }
     
     
@@ -44,28 +37,12 @@ public class Animal {
         this.cod_animal = cod_animal;
     }
 
-    public Date getData_nasc_animal() {
-        return data_nasc_animal;
-    }
-
-    public void setData_nasc_animal(Date data_nasc_animal) {
-        this.data_nasc_animal = data_nasc_animal;
-    }
-
-    public String isSexo_animal() {
+    public String getSexo_animal() {
         return sexo_animal;
     }
 
-    public void setSexo_animal(boolean sexo_animal) {
+    public void setSexo_animal(String sexo_animal) {
         this.sexo_animal = sexo_animal;
-    }
-
-    public String getEspecie_animal() {
-        return especie_animal;
-    }
-
-    public void setEspecie_animal(String especie_animal) {
-        this.especie_animal = especie_animal;
     }
 
     public String getRaca_animal() {
@@ -76,21 +53,6 @@ public class Animal {
         this.raca_animal = raca_animal;
     }
 
-    public float getPeso_animal() {
-        return peso_animal;
-    }
-
-    public void setPeso_animal(float peso_animal) {
-        this.peso_animal = peso_animal;
-    }
-
-    public String getObs_gerais() {
-        return obs_gerais;
-    }
-
-    public void setObs_gerais(String obs_gerais) {
-        this.obs_gerais = obs_gerais;
-    }
     
     public void cadastrarAnimal(Animal animal){};
     
