10cdae01e3cc5d8bacba08def75a9f28a66efbdb
DrLighting <95546918+DrLighting@users.noreply.github.com>
2021-12-08 04:10:42 -0300

    Add files via upload

--- scriptsql.sql
+++ scriptsql.sql
@@ -1,62 +1,43 @@
-drop database if exists Digipets;
 CREATE DATABASE Digipets;
 USE Digipets;
 
 create table animal(
-	nomeAnimal varchar(45),
-	codAnimal int,
-	dataNascimentoAnimal varchar(45),
-	sexoAnimal varchar(45),
-	especieAnimal varchar(45),
-	racaAnimal varchar(45),
-	pesoAnimal float,
-	obsGerais varchar(45),
-    codTutor int
+	nome_animal varchar(45),
+	cod_animal int,
+    especie_animal varchar(45),
+    primary key (cod_animal)
 );
 
 create table vacina(
-	nomeVacina varchar(45),
-	codVacina int,
-	dataAplicVacina varchar(45),
-	loteVacina varchar(45),
-	dataFabVacina varchar(45)
+	nome_vacina varchar(45),
+	cod_vacina int,
+	data_aplic_vacina date,
+    primary key (cod_vacina)
 );
 
 create table veterinario(
-	nomeVet varchar(45),
-	emailVet varchar(45),
-	telefoneVet varchar(45),
-	crmvVet varchar(45),
-	cpfUsuario varchar(45),
-	senhaUsuario varchar(45),
-	tipoUsuario varchar(45)
+	nome_vet varchar(45),
+	cpf_usuario varchar(45),
+	senha_usuario varchar(45),
+	tipo_usuario varchar(45),
+    primary key (cpf_usario)
 );
 
 create table tutor(
-	nomeTutor varchar(45),
-	codTutor int,
-	emailTutor varchar(45),
-	telefoneTutor varchar(45),
-	enderecoTutor varchar(45),
-	cpfUsuario varchar(45),
-	senhaUsuario varchar(45),
-	tipoUsuario varchar(45)
+	nome_tutor varchar(45),
+	cpf_usuario varchar(45),
+	senha_usuario varchar(45),
+	tipo_usuario varchar(45),
+    primary key (cpf_usuario)
 );
 
 CREATE TABLE carteira(
-  codAnimal INT,
-  nomeAnimal VARCHAR(45),
-  dataNascianimal VARCHAR(45),
-  sexoAnimal VARCHAR(45),
-  racaAnimal VARCHAR(45),
-  pesoAnimal FLOAT,
-  obsGerais VARCHAR(45),
-  nomeVacina VARCHAR(45),
-  codVacina VARCHAR(45),
-  Carteiracol INT,
-  dataAplicVacina VARCHAR(45),
-  loteAplicVacina VARCHAR(45),
-  dataFebVacina VARCHAR(45),
-  tutor_codTutor INT,
-  veterianrio_cpfVet VARCHAR(45)
-  );
+  nome_animal varchar(45),
+  nome_vacina varchar(45),
+  data_aplic_vacina date,
+  nome_tutor varchar(45),
+  nome_vet varchar(45),
+  primary key (nome_animal)
+);
+  
+
