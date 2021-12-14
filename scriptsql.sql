8c53c0ed34df29db6b970e2f88659bb906f2722b
DrLighting <95546918+DrLighting@users.noreply.github.com>
2021-12-14 17:42:57 -0300

    Add files via upload

--- scriptsql.sql
+++ scriptsql.sql
@@ -6,39 +6,45 @@ create table animal(
 	cod_animal int,
     raca_animal varchar(45),
     sexo_animal varchar(45),
-    primary key (cod_animal)
+    especie_animal varchar(45),
+    obsGerais_animal varchar(500),
+    primary key (cod_animal),
+    constraint fk_cpf_tutor foreign key (cpf_tutor) references tutor (cpf_tutor),
+    constraint fk_cpf_vet foreign key (cpf_vet) references veterianrio (cpf_vet)
 );
 
 create table vacina(
 	nome_vacina varchar(45),
 	cod_vacina int,
 	data_aplic_vacina date,
-    primary key (cod_vacina)
+    primary key (cod_vacina),
+    constraint fk_cpf_vet foreign key (cpf_vet) references veterinario (cpf_vet) 
 );
 
 create table veterinario(
 	nome_vet varchar(45),
-	cpf_usuario varchar(45),
-	senha_usuario varchar(45),
+	cpf_vet varchar(45),
+	senha_vet varchar(45),
 	tipo_usuario varchar(45),
-    primary key (cpf_usario)
+    primary key (cpf_vet)
 );
 
 create table tutor(
 	nome_tutor varchar(45),
-	cpf_usuario varchar(45),
-	senha_usuario varchar(45),
-	tipo_usuario varchar(45),
-    primary key (cpf_usuario)
+	cpf_tutor varchar(45),
+	senha_tutor varchar(45),
+	tipo_tutor varchar(45),
+    primary key (cpf_tutor),
+    constraint fk_cpf_vet foreign key (cpf_vet) references veterianrio (cpf_vet)
 );
 
 CREATE TABLE carteira(
   nome_animal varchar(45),
   nome_vacina varchar(45),
   data_aplic_vacina date,
-  nome_tutor varchar(45),
-  nome_vet varchar(45),
-  primary key (nome_animal)
+  primary key (nome_animal),
+  constraint fk_nome_tutor foreign key (nome_tutor) references tutor (nome_tutor),
+  constraint fk_nome_vet foreign key (nome_vet) references veterianrio (nome_vet)
 );
   
 
