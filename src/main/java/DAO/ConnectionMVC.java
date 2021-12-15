0c77e55a75768f3d22536e02db07900c9f86001e
Neto Polacchini <netopolacchini@gmail.com>
2021-12-15 10:44:11 -0300

    15/12 10:44

--- src/main/java/DAO/ConnectionMVC.java
+++ src/main/java/DAO/ConnectionMVC.java
@@ -7,18 +7,18 @@ public class ConnectionMVC {
 private static Connection connection = null;
 
     public static Connection getConnection() {
-  /*
-        try {
+  
+       /* try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/armario?useSLL=false", "root", "");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/digipets", "root", "");
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-*/
+        }*/
+
   if (connection == null){
    try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -26,7 +26,7 @@ private static Connection connection = null;
             e.printStackTrace();
         }
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/armario?useSLL=false", "root", "");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/digipets", "root", "");
         } catch (SQLException e) {
             e.printStackTrace();
         } 
