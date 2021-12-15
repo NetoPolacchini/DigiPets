9698bff2492778692c922903ddd9c9ea1ecf60bc
Neto Polacchini <netopolacchini@gmail.com>
2021-12-14 22:34:44 -0300

    lastGit

--- src/main/java/View/GerenciarAnimal.java
+++ src/main/java/View/GerenciarAnimal.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package View;
 import DAO.ExceptionDAO;
 import java.awt.BorderLayout;
@@ -12,10 +7,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JPanel;
 
-/**
- *
- * @author joao_
- */
+
 public class GerenciarAnimal extends javax.swing.JPanel {
 
     /**
